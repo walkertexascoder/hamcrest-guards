a3f6b0f355a257f6c34c561c7800bcba59e8cd8b
Don Walker <don@donaldewalker.com>
2010-01-31 19:40:28 -0600

    Adopt different coding standards

--- src/main/java/com/donaldewalker/hamcrestguards/matchers/MatchedBy.java
+++ src/main/java/com/donaldewalker/hamcrestguards/matchers/MatchedBy.java
@@ -6,45 +6,36 @@ import org.hamcrest.Description;
 import org.hamcrest.TypeSafeMatcher;
 
 /**
- * Hamcrest matcher that verifies a string matches a given
- * regular expression. Supports all regular expression supported
- * by the Java core library.
+ * Hamcrest matcher that verifies a string matches a given regular expression.
+ * Supports all regular expression supported by the Java core library.
  * 
  * @author "Don Walker (don@donaldewalker.com)"
- *
  */
-public class MatchedBy extends TypeSafeMatcher<String>
-{
+public class MatchedBy extends TypeSafeMatcher<String> {
    private Pattern pattern;
-   
-   public MatchedBy(String regex)
-   {
+
+   public MatchedBy(String regex) {
       setPattern(Pattern.compile(regex));
    }
-   
-   public static MatchedBy matchedBy(String regex)
-   {
+
+   public static MatchedBy matchedBy(String regex) {
       return new MatchedBy(regex);
    }
-   
+
    @Override
-   public boolean matchesSafely(String item)
-   {
+   public boolean matchesSafely(String item) {
       return getPattern().matcher(item).matches();
    }
 
-   public void describeTo(Description description)
-   {
+   public void describeTo(Description description) {
       description.appendText("string matching ").appendValue(pattern);
    }
-   
-   private void setPattern(Pattern pattern)
-   {
+
+   private void setPattern(Pattern pattern) {
       this.pattern = pattern;
    }
 
-   public Pattern getPattern()
-   {
+   public Pattern getPattern() {
       return pattern;
    }
 }
--- src/test/java/com/donaldewalker/hamcrestguards/HamcrestGuardsTest.java
+++ src/test/java/com/donaldewalker/hamcrestguards/HamcrestGuardsTest.java
@@ -12,60 +12,47 @@ import org.junit.Test;
 
 /**
  * @author "Don Walker (don@donaldewalker.com)"
- *
  */
-public class HamcrestGuardsTest
-{
+public class HamcrestGuardsTest {
    @Test
-   public void should_not_throw_an_exception_if_requirement_is_met() throws Exception
-   {
+   public void should_not_throw_an_exception_if_requirement_is_met() throws Exception {
       requireThat(null, is(nullValue()));
    }
-   
+
    @Test(expected = IllegalArgumentException.class)
-   public void should_throw_illegal_argument_exception_if_requirement_not_met() throws Exception
-   {
+   public void should_throw_illegal_argument_exception_if_requirement_not_met() throws Exception {
       requireThat(null, is(notNullValue()));
    }
-   
+
    @Test
-   public void should_provide_named_field_in_first_part_of_exception_text() throws Exception
-   {
-      try
-      {
+   public void should_provide_named_field_in_first_part_of_exception_text() throws Exception {
+      try {
          requireThat(null, "alazar", is(notNullValue()));
          fail("Expected illegal argument exception");
       }
-      catch (IllegalArgumentException e)
-      {
+      catch (IllegalArgumentException e) {
          assertThat(e.getMessage(), startsWith("alazar"));
       }
    }
-   
+
    @Test
-   public void should_throw_illegal_argument_exception_if_matcher_is_null() throws Exception
-   {
-      try
-      {
+   public void should_throw_illegal_argument_exception_if_matcher_is_null() throws Exception {
+      try {
          requireThat(null, null);
          fail("Expected illegal argument exception");
       }
-      catch (IllegalArgumentException e)
-      {
+      catch (IllegalArgumentException e) {
          assertThat(e.getMessage(), is("matcher may not be null"));
       }
    }
-   
+
    @Test
-   public void should_throw_illegal_argument_exception_if_object_name_is_null() throws Exception
-   {
-      try
-      {
+   public void should_throw_illegal_argument_exception_if_object_name_is_null() throws Exception {
+      try {
          requireThat(null, null, is(nullValue()));
          fail("Expected illegal argument exception");
       }
-      catch (IllegalArgumentException e)
-      {
+      catch (IllegalArgumentException e) {
          assertThat(e.getMessage(), is("object name may not be null"));
       }
    }
