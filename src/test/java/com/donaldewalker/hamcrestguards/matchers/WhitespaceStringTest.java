065ce2d455b2a7e68d0198b42bf02134ca1e7ca7
Don Walker <don@donaldewalker.com>
2010-02-08 22:56:34 -0600

    Add WhitespaceString matcher

---
+++ src/test/java/com/donaldewalker/hamcrestguards/matchers/WhitespaceStringTest.java
@@ -0,0 +1,21 @@
+package com.donaldewalker.hamcrestguards.matchers;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * @author Don Walker (don@walkertexascoder.com)
+ */
+public class WhitespaceStringTest {
+   @Test
+   public void should_not_match_a_string_that_contains_a_non_whitespace_character() throws Exception {
+      assertFalse(new WhitespaceString().matches("a\nb "));
+   }
+   
+   @Test
+   public void should_match_a_string_that_contains_only_whitespace_characters() throws Exception {
+      assertTrue(new WhitespaceString().matches("   \t\n "));
+   }
+}
